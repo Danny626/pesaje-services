2f8be5deb9c9d80fea711284498ce052a90e2a52
Danny <live626@gmail.com>
2022-10-27 11:17:11 -0400

    initializing project

---
+++ src/main/java/com/albo/pesajeservices/PesajeServicesApplication.java
@@ -0,0 +1,13 @@
+package com.albo.pesajeservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesajeServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesajeServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albo/pesajeservices/PesajeServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albo.pesajeservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesajeServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
